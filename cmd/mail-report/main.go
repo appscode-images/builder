060e5b94588b5d4111f6a6245e7c82a4d7caed16
Tamal Saha <tamal@appscode.com>
2023-10-11 14:00:28 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmd/mail-report/main.go
+++ cmd/mail-report/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	flag "github.com/spf13/pflag"
+)
 
+func main() {
+	var name *string = flag.String("name", "", "Name of binary")
+
+	flag.Parse()
+
+	fmt.Println("EMAIL BUILD REPORT for " + *name)
 }
