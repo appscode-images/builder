01128ccfcbb37377496068ec1739d23630a8fc9e
pritamdas99 <pritam@appscode.com>
2024-11-07 18:52:45 +0600

    Update gitcommits
    Signed-off-by: pritamdas99 <pritam@appscode.com>

--- cmd/refresh-library/main.go
+++ cmd/refresh-library/main.go
@@ -34,6 +34,7 @@ func main() {
 		"logstash",
 		"opensearch",
 		"opensearch-dashboards",
+		"solr",
 	}
 	flag.StringSliceVar(&skipList, "skip", skipList, "Skip official image (because manually maintained)")
 	flag.Parse()
