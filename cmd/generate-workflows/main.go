c2c1d85c5017c955caa4c1047cffe0ebb096b181
Tamal Saha <tamal@appscode.com>
2025-09-05 14:05:45 +0600

    Update workflow
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmd/generate-workflows/main.go
+++ cmd/generate-workflows/main.go
@@ -173,8 +173,8 @@ jobs:
         # wget https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.deb
         # sudo dpkg -i trivy_0.18.3_Linux-64bit.deb
         sudo apt-get install -y --no-install-recommends wget apt-transport-https gnupg lsb-release
-        wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-        echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+        wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+        echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
         sudo apt-get update
         sudo apt-get install -y --no-install-recommends trivy
 
